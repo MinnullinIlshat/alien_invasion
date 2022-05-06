a9c954517a662b8df2120a540bca9239d1fcdbbb
Ilshat Minnullin <minnullinilshat@gmail.com>
2022-05-06 17:32:46 +0300

    060622

--- alien_invasion.py
+++ alien_invasion.py
@@ -20,16 +20,20 @@ class AlienInvasion:
     def run_game(self):
         """Запуск основного цикла игры"""
         while True:
-            # Отслеживание событий клавиатуры и мыши
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
+            self._update_screen()
+    
+    def _check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
 
-            # При каждом проходе цикла перерисовывается экран
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
-            # отображение последнего прорисованного экрана
-            pygame.display.flip()
+    def _update_screen(self):
+        """Обновляет изображения на экране и отображает новый экран."""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        pygame.display.flip()
 
 if __name__ == '__main__':
     # Создание экземпляра запуска игры
